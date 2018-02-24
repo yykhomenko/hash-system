54f7bb98892134d3282e5b3c4ea4f628fa1f2268
yykhomenko <yykhomenko@gmail.com>
2018-02-24 23:36:36 +0200

    simplify

--- src/test/scala/system/hash/MetricRouteTest.scala
+++ src/test/scala/system/hash/MetricRouteTest.scala
@@ -1,6 +1,5 @@
 package system.hash
 
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.headers.`WWW-Authenticate`
 import akka.http.scaladsl.server.Route
--- src/test/scala/system/hash/json/JsonMsisdnTest.scala
+++ src/test/scala/system/hash/json/JsonMsisdnTest.scala
@@ -1,6 +1,5 @@
 package system.hash.json
 
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.headers.`WWW-Authenticate`
 import akka.http.scaladsl.server.Route
