a77b3d5ce66f413a1caea403317fb9842efc6528
yykhomenko <yykhomenko@gmail.com>
2021-12-26 12:55:09 +0200

    migrate to new akka http interface

--- src/main/scala/system/hash/App.scala
+++ src/main/scala/system/hash/App.scala
@@ -1,21 +1,18 @@
 package system.hash
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.server.HttpApp
+import akka.http.scaladsl.Http
 import system.hash.actor.MetricController
-import system.hash.actor.MetricController.MetricsReq
 import system.hash.route.Routes
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
 
-object App extends HttpApp with Routes {
-
-  val system = ActorSystem("hash-system")
+object App extends App with Routes {
+  implicit val system: ActorSystem = ActorSystem("hash-system")
   val metric = system.actorOf(MetricController.props, "metric-controller")
 
-  def main(args: Array[String]) = {
-    withTimer("start load hashes", loadHashes())
-    startServer("0.0.0.0", 8080, system)
-  }
+  withTimer("start load hashes", loadHashes())
+  Http().newServerAt("0.0.0.0", 8080).bind(routes)
+  Await.result(system.whenTerminated, Duration.Inf)
 }
